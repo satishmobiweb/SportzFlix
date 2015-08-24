a948b30231f4535566ab76512ee249cee86ad162
forgehead <jf245@buffalo.edu>
2015-08-24 14:14:31 -0700

    Initial commit

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name sportzflixApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the sportzflixApp
+ */
+angular.module('sportzflixApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
